443907a316b654f69547e8548c031f2319061398
stanley <stanley.njuguna@student.moringaschool.com>
2024-01-15 02:45:15 +0300

    assignment done

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Add your code here
+
+
+function submitData(name, email) {
+    const reqBody = {
+        name: name,
+        email: email
+    };
+
+    fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            Accept: "application/json",
+        },
+        body: JSON.stringify(reqBody)
+    })
+    .then(response => response.json())
+    .then(data => functionData(data)
+        )
+    .catch(error => {
+        console.error('Error sending data:', error);
+    });
+}
+function functionData(data){
+    const newid =data.id;
+    const createid=document.createElement('p')
+    createid.textContent=`New ID: ${newid}`;
+
+      document.body.appendChild(createid);
+}
+
+// Example usage:
+submitData('Stanley', 'njugunastanleyy@gmail.com'); 
